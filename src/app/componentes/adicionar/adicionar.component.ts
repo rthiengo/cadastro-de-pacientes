e1f5397b01e38c9a018e6cac71f45df3b43a6813
rthiengo <rthiengo@hotmail.com>
2023-11-06 18:18:54 -0300

    modificações

--- src/app/componentes/adicionar/adicionar.component.ts
+++ src/app/componentes/adicionar/adicionar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-adicionar',
   templateUrl: './adicionar.component.html',
-  styleUrls: ['./adicionar.component.css']
+  styleUrls: ['./adicionar.component.css'],
 })
 export class AdicionarComponent {
+  adicionarPaciente() {
+    alert('Novo Paciente Criado!');
+  }
 
+  cancelar() {
+    alert('Ação cancelada');
+  }
 }
